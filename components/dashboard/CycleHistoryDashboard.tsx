241364a0798517ff343ba1e616ea053c5d0c16e8
Tazmania2 <TCRabelo@gmail.com>
2025-10-20 10:56:29 -0300

    Update CycleHistoryDashboard.tsx

--- components/dashboard/CycleHistoryDashboard.tsx
+++ components/dashboard/CycleHistoryDashboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { CycleHistoryData } from '../../types';
 import { historyService } from '../../services/history.service';
 import { LoadingState, Skeleton } from '../ui/LoadingSpinner';
